5fdcc0bd822428213d94a319d0a3dfd20e6eaf30
swilfling <sandra.wilfling@student.tugraz.at>
2022-12-02 14:40:49 +0100

    add: example for FMPy simulation

--- Simulator/fmpySimulator.py
+++ Simulator/fmpySimulator.py
@@ -1,7 +1,7 @@
 import numpy as np
-import numpy.lib.recfunctions as rfs
 import fmpy
 import fmpy.fmi2
+import numpy.lib.recfunctions as rfs
 from pandas import DataFrame
 from .ModelicaSimulator import ModelicaSimulator
 
@@ -12,34 +12,27 @@ class FMPYSimulator(ModelicaSimulator):
     """
     fmu_dir = ""
     fmu_filename = ""
-    data_filename = ""
     input_data = None
     input_feature_names = []
     output_feature_names = []
     simulation_results = None
     fmu = None
 
-    def __init__(self, fmu_filename="", input_feature_names=None, output_feature_names=None, data_filename="", **kwargs):
+    def __init__(self, fmu_filename="", input_feature_names=None, output_feature_names=None, input_data=None, **kwargs):
         super().__init__(**kwargs)
-        self.data_filename = data_filename
+        self.input_data = input_data
         self.fmu_filename = fmu_filename
         self.input_feature_names = input_feature_names
         self.output_feature_names = output_feature_names
 
     def _get_simulation_results(self, trajectory_names, **kwargs):
-        simulation_results = {"Time":np.array(self.simulation_results["Time"])}
+        simulation_results = {}
         for name in trajectory_names:
             simulation_results.update({name: np.array(self.simulation_results[name])})
-            if len(self.simulation_results.size) > self.input_data.shape[0]:
+            simulation_results.update({f"data_{name}": np.array(self.input_data[name])})
+            if self.simulation_results.size > self.input_data.shape[0]:
                 raise Exception("Sizes of simulation results and input data do not match.")
-        return DataFrame(simulation_results[trajectory_names],index=simulation_results["Time"], columns=trajectory_names)
-
-    def _create_simulation_input_from_file(self, data_filename, skip_rows=4):
-        input_data = np.genfromtxt(data_filename, delimiter=';', names=True)
-        input_data = rfs.rename_fields(input_data,{"Zeitraum":"Time"})
-        pick_columns = ["Time"] + self.input_feature_names + self.output_feature_names
-        rows = input_data[pick_columns]
-        return rows[skip_rows-1:]
+        return DataFrame(simulation_results, index=np.array(self.simulation_results["Time"]))
 
     def _create_start_values(self, input_data):
         return{name: input_data[name] for name in self.input_feature_names + self.output_feature_names}
@@ -56,7 +49,6 @@ class FMPYSimulator(ModelicaSimulator):
 
     def _init_experiment(self, exp_name="", **kwargs):
         self.fmu = self._extract_and_instantiate_FMU("FMU1")
-        self.input_data = self._create_simulation_input_from_file(self.data_filename)
         init_values = self.input_data[0] if not self.init_params.use_init_values else list(self.init_params.init_variables.values())
         self.start_values = self._create_start_values(init_values)
 
